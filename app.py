5c592d05c3600bca64c25a4f2ea24353bbf205c1
unknown <robert@ekholmgroup.fi>
2022-05-06 17:25:21 +0300

    Slight changes to code layout

--- app.py
+++ app.py
@@ -7,34 +7,35 @@ ticker = input().upper()
 
 print("Choose currency EURO - 0 / USD - 1")
 choice = input()
-currency = ['EUR', 'USD']
+currency = ["EUR", "USD"]
 
 # Kraken API
 krakenResp = requests.get(
-    f'https://api.kraken.com/0/public/Ticker?pair={ticker}{currency[int(choice)]}')
+    f"https://api.kraken.com/0/public/Ticker?pair={ticker}{currency[int(choice)]}"
+)
 krakenData = krakenResp.json()
 
 
-keyData = krakenData['result'][list(krakenData['result'].keys())[0]]
+keyData = krakenData["result"][list(krakenData["result"].keys())[0]]
 
-displayNames = {'a': 'Ask',
-                'b': 'Bid',
-                'c': 'Last trade closed',
-                'v': 'Volume',
-                'p': 'Volume weighted average price',
-                't': 'Number of trades',
-                'l': 'Low',
-                'h': 'High',
-                'o': 'Todays opening price',
-                }
+displayNames = {
+    "a": "Ask",
+    "b": "Bid",
+    "c": "Last trade closed",
+    "v": "Volume",
+    "p": "Volume weighted average price",
+    "t": "Number of trades",
+    "l": "Low",
+    "h": "High",
+    "o": "Todays opening price",
+}
 
 
 # def zipper(elem):
 #     return {displayNames[elem]:keyData[elem]}
 # tickerData = map(zipper, list(keyData.keys()))
 
-mapData = map(
-    lambda elem: {displayNames[elem]: keyData[elem]}, list(keyData.keys()))
+mapData = map(lambda elem: {displayNames[elem]: keyData[elem]}, list(keyData.keys()))
 listData = list(mapData)
 
 
